efed7967f07681a213d3da14d95a8ca2ecf06057
Carlos <carlos.oli.h23@gmail.com>
2025-12-14 16:43:35 -0300

    Básico da classe reserva feito.

--- src/entities/Reserva.java
+++ src/entities/Reserva.java
@@ -1,4 +1,54 @@
 package entities;
 
 public class Reserva {
+
+    private String nomeHospede;
+    private String quartoHotel;
+    private int quantidadeDias;
+    private double valorTotalHospedagem;
+
+    public Reserva(String nomeHospede, String quartoHotel, int quantidadeDias) {
+        this.nomeHospede = nomeHospede;
+        this.quartoHotel = quartoHotel;
+        this.quantidadeDias = quantidadeDias;
+    }
+
+    public String getNomeHospede() {
+        return nomeHospede;
+    }
+    public void setNomeHospede(String nomeHospede) {
+        this.nomeHospede = nomeHospede;
+    }
+
+    public String getQuartoHotel() {
+        return quartoHotel;
+    }
+    public void setQuartoHotel(String quartoHotel) {
+        this.quartoHotel = quartoHotel;
+    }
+
+    public int getQuantidadeDias() {
+        return quantidadeDias;
+    }
+    public void setQuantidadeDias(int quantidadeDias) {
+        this.quantidadeDias = quantidadeDias;
+    }
+
+    public double getValorTotalHospedagem() {
+        return valorTotalHospedagem;
+    }
+
+    public void calcularValorTotal() {
+    }
+
+    public String toString() {
+        return "\nNome Hóspede: " +
+                getNomeHospede() +
+                "\nQuarto Escolhido: " +
+                getQuartoHotel() +
+                "\nQuantidade de Dias Reservado: " +
+                getQuantidadeDias() +
+                "\nValor Total da Hospedagem: " +
+                getValorTotalHospedagem();
+    }
 }
