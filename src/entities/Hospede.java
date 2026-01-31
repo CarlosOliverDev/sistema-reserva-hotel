9e0b1e81909e9f85e22c3f54e2c454084b1db081
Carlos <carlos.oli.h23@gmail.com>
2026-01-31 18:43:22 -0300

    Métodos e atributos adicionados no enum TipoQuarto e na classe Hospede.

--- src/entities/Hospede.java
+++ src/entities/Hospede.java
@@ -1,4 +1,43 @@
 package entities;
 
 public class Hospede {
+    private String nomeHospede;
+    private int idadeHospede;
+    private String identidadeHospede;
+
+    public Hospede(String nomeHospede, int idadeHospede, String identidadeHospede) {
+        this.nomeHospede = nomeHospede;
+        this.idadeHospede = idadeHospede;
+        this.identidadeHospede = identidadeHospede;
+    }
+
+    public String getNomeHospede() {
+        return nomeHospede;
+    }
+    public void setNomeHospede(String nomeHospede) {
+        this.nomeHospede = nomeHospede;
+    }
+
+    public int getIdadeHospede() {
+        return idadeHospede;
+    }
+    public void setIdadeHospede(int idadeHospede) {
+        this.idadeHospede = idadeHospede;
+    }
+
+    public String getIdentidadeHospede() {
+        return identidadeHospede;
+    }
+    public void setIdentidadeHospede(String identidadeHospede) {
+        this.identidadeHospede = identidadeHospede;
+    }
+
+    @Override
+    public String toString() {
+        return "Hospede:\n" +
+                "Nome: " + nomeHospede +
+                ", Idade: " + idadeHospede +
+                ", Identidade: " + identidadeHospede +
+                '\n';
+    }
 }
--- src/entities/TipoQuarto.java
+++ src/entities/TipoQuarto.java
@@ -1,4 +1,23 @@
 package entities;
 
 public enum TipoQuarto {
+    QUARTOBASICO("Quarto Básico", 150.00),
+    QUARTOLUXO("Quarto de Luxo", 830.00),
+    QUARTOPRESIDENCIAL("Quarto Presidencial", 10000.00);
+
+    private final String nomeQuarto;
+    private final double valorQuarto;
+
+    TipoQuarto(String nomeQuarto, double valorQuarto) {
+        this.nomeQuarto = nomeQuarto;
+        this.valorQuarto = valorQuarto;
+    }
+
+    public String getNomeQuarto() {
+        return nomeQuarto;
+    }
+
+    public double getValorQuarto() {
+        return valorQuarto;
+    }
 }
